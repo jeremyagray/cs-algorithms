a5712e17cbd3169760fda12763daf68f8b3f99e0
Jeremy A Gray <gray@flyquackswim.com>
2024-11-02 19:49:27 -0500

    feat: create repository
    Signed-off-by: Jeremy A Gray <gray@flyquackswim.com>

---
+++ csalgo/sorting.py
@@ -0,0 +1,13 @@
+# ******************************************************************************
+#
+# cs-algorithms, basic algorithms in Python
+#
+# Copyright 2024 Jeremy A Gray <gray@flyquackswim.com>.
+#
+# All rights reserved.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# ******************************************************************************
+
+"""Sorting algorithms."""
---
+++ tests/test_sorting.py
@@ -0,0 +1,20 @@
+# ******************************************************************************
+#
+# cs-algorithms, basic algorithms in Python
+#
+# Copyright 2024 Jeremy A Gray <gray@flyquackswim.com>.
+#
+# All rights reserved.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# ******************************************************************************
+
+"""Sorting tests."""
+
+# from csalgo import sort
+
+
+# def test_repr_should_work():
+#     """Should be default ``repr``."""
+#     assert repr(Template()) == "Template()"
